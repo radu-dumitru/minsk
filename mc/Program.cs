d5fc909ba3beead4815bea912b8451fca74260f5
Radu Dumitru <radu.dumitru05@gmail.com>
2023-11-26 13:22:08 +0200

    commit

---
+++ mc/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace mc 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            while (true) 
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+                    
+                    Console.Write($"{token.Kind}: {token.Text}");
+
+                    if (token.Value != null)
+                        Console.Write($" {token.Value}");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParanthesisToken,
+        CloseParanthesisToken,
+        BadToken,
+        EndOfFileToken
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; }
+    }
+
+    class Lexer 
+    {
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        private char Current
+        {
+            get 
+            {
+                if (_position >= _text.Length)
+                    return '\0';
+
+                return _text[_position];
+            }
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            // <numbers>
+            // + - * / ( )
+            // <whitespace>
+
+            if (_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+
+            if (char.IsDigit(Current))
+            {
+                var _start = _position;
+
+                while (char.IsDigit(Current))
+                    Next();
+
+                var length = _position - _start;
+                var text = _text.Substring(_start, length);
+                int.TryParse(text, out var value);
+
+                return new SyntaxToken(SyntaxKind.NumberToken, _start, text, value);
+            }
+
+            if (char.IsWhiteSpace(Current))
+            {
+                var _start = _position;
+
+                while (char.IsWhiteSpace(Current))
+                    Next();
+
+                var length = _position - _start;
+                var text = _text.Substring(_start, length);
+
+                return new SyntaxToken(SyntaxKind.WhitespaceToken, _start, text, null);
+            }
+
+            if (Current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            else if (Current == '-')
+                 return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            else if (Current == '*')
+                 return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+            else if (Current == '/')
+                 return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+            else if (Current == '(')
+                 return new SyntaxToken(SyntaxKind.OpenParanthesisToken, _position++, "(", null);
+             else if (Current == ')')
+                 return new SyntaxToken(SyntaxKind.CloseParanthesisToken, _position++, ")", null);
+
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+    }
+
+    class Parser
+    {
+        private readonly SyntaxToken[] _tokens;
+        private int _position;
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
+
+            var lexer = new Lexer(text);
+            SyntaxToken token;
+
+            do
+            {
+                token = lexer.NextToken();
+
+                if (token.Kind != SyntaxKind.WhitespaceToken && token.Kind != SyntaxKind.BadToken)
+                {
+                    tokens.Add(token);
+                }
+            } while (token.Kind != SyntaxKind.EndOfFileToken);
+        }
+
+        private SyntaxToken Peek(int offset)
+        {
+            var index = _position + offset;
+
+            if (index >= _tokens.Length)
+                return _tokens[_tokens.Length - 1];
+
+            return _tokens[index];
+        }
+    }
+}
